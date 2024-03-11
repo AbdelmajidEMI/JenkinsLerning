a8a0aa4609bb8790ad6d767a915bac83f5e2196a
Abood <hou.abd.2015@gmail.com>
2024-03-11 11:30:26 +0100

    All good

--- script.sh
+++ script.sh
@@ -1,2 +1,9 @@
-echo "Hello world" >> output.txt
-echo "This is a new line " > output.txt
+# This is a bash script that writes text to a file named 'output.txt'
+
+# The '>>' operator is used to append text to the file
+echo "Hello world" > output.txt
+
+# The '>' operator is used to overwrite the file with new text
+# In this case, the file 'output.txt' is overwritten twice with the same text
+echo "This is a new line " >> output.txt
+echo "This is a new line " >> output.txt
