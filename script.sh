b0f6761be77fed697c8239c5a7bd5dc7ace8bcf2
Abood <hou.abd.2015@gmail.com>
2024-03-11 11:33:52 +0100

    Adding laamiri

--- script.sh
+++ script.sh
@@ -6,4 +6,4 @@ echo "Hello world" > output.txt
 # The '>' operator is used to overwrite the file with new text
 # In this case, the file 'output.txt' is overwritten twice with the same text
 echo "This is a new line " >> output.txt
-echo "This is a new line " >> output.txt
+echo "This is laamiri " >> output.txt
